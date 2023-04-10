d9a5180dacbe9c87013ca704ac2a2bc5bde6a7c2
Hunter Andersen <andersen.hunter42@gmail.com>
2023-04-10 08:46:51 -0700

    Finished the exercises

--- app.js
+++ app.js
@@ -4,7 +4,38 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Helen";
+let lastName = "Parr";
+var age = 38;
 // Exercise 2
-
+lastName = "Parr";
+age = 40;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(firstName, lastName, age);
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(price, isOnSale, salePercentage, stock, inStock, selectedSize);
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(title, author, pageCount);
+console.log(bookmark, hasRead);
+
+//String Interpolation
+console.log(`The book, ${title}, by ${author} is ${pageCount} pages long`);
